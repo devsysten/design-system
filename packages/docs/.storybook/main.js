2d0064380995540efaf064d8c4026245f067c451
devsysten <tec.santi2009@gmail.com>
2023-06-09 17:52:07 -0300

    correção node modules

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -5,7 +5,6 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-docs",
     "@storybook/addon-a11y",
   ],
   framework: {
